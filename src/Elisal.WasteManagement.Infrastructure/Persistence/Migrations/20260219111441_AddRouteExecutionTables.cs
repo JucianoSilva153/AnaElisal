0b0f9d7fab8fb71f124db7c54cb0e2be6bbffd65
JucianoSilva153 <jucianodasilvasilvaa@gmail.com>
2026-02-19 12:16:07 +0100

    feat: add database tables for route execution, vehicles, and drivers.

---
+++ src/Elisal.WasteManagement.Infrastructure/Persistence/Migrations/20260219111441_AddRouteExecutionTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elisal.WasteManagement.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRouteExecutionTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
