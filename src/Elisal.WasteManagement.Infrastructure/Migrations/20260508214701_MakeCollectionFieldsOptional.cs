40f8e234e42fc1b163dabd05a75dd31da2d2cc9a
JucianoSilva153 <jucianodasilvasilvaa@gmail.com>
2026-05-08 23:23:11 +0100

    fix, last updates

--- src/Elisal.WasteManagement.Infrastructure/Migrations/20260508214701_MakeCollectionFieldsOptional.cs
+++ src/Elisal.WasteManagement.Infrastructure/Migrations/20260508214701_MakeCollectionFieldsOptional.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,26 +10,6 @@ namespace Elisal.WasteManagement.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<bool>(
-                name: "IsSkipped",
-                table: "RoutePointExecutionStatuses",
-                type: "tinyint(1)",
-                nullable: false,
-                defaultValue: false);
-
-            migrationBuilder.AddColumn<string>(
-                name: "SkipReason",
-                table: "RoutePointExecutionStatuses",
-                type: "longtext",
-                nullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4");
-
-            migrationBuilder.AddColumn<int>(
-                name: "AssignedDriverId",
-                table: "Rota",
-                type: "int",
-                nullable: true);
-
             migrationBuilder.AlterColumn<int>(
                 name: "tipo_residuo_id",
                 table: "RegistoRecolha",
@@ -45,43 +25,11 @@ namespace Elisal.WasteManagement.Infrastructure.Migrations
                 nullable: true,
                 oldClrType: typeof(double),
                 oldType: "double");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Rota_AssignedDriverId",
-                table: "Rota",
-                column: "AssignedDriverId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Rota_Usuario_AssignedDriverId",
-                table: "Rota",
-                column: "AssignedDriverId",
-                principalTable: "Usuario",
-                principalColumn: "id");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Rota_Usuario_AssignedDriverId",
-                table: "Rota");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Rota_AssignedDriverId",
-                table: "Rota");
-
-            migrationBuilder.DropColumn(
-                name: "IsSkipped",
-                table: "RoutePointExecutionStatuses");
-
-            migrationBuilder.DropColumn(
-                name: "SkipReason",
-                table: "RoutePointExecutionStatuses");
-
-            migrationBuilder.DropColumn(
-                name: "AssignedDriverId",
-                table: "Rota");
-
             migrationBuilder.AlterColumn<int>(
                 name: "tipo_residuo_id",
                 table: "RegistoRecolha",
